8179d83b2402ded9398933125bc7ca4f5d6f5294
Martin Bless <martin@mbless.de>
2016-01-04 22:32:45 +0100

    ...

--- README.rst
+++ README.rst
@@ -2,5 +2,112 @@
 Collect Parts and Pieces to Start New Python Projects
 =====================================================
 
+.. default-role:: code
+.. highlight:: bash
 
+Readings
+========
+
+Inspired by `The Elements of Python Style <https://github.com/amontalenti/elements-of-python-style>`__.
+See http://mbless.de/blog/2015/01/15/looking-for-wisdom-about-python.html#python-coding-style
+
+
+Remember
+========
+
+::
+
+   virtualenv venv
+   source venv/bin/activate
+
+
+   # http://nvie.com/posts/better-package-management/
+
+   pip install -r            requirements.in
+   pip install --requirement requirements.in
+
+   pip freeze > requirements.txt
+
+
+Development::
+
+   pip install --upgrade --requirement requirements.in; pip freeze >requirements.txt
+
+Production::
+
+   pip install --requirement requirements..txt
+
+
+
+Common Thirdparty Packages
+==========================
+
+
+
+docopt
+------
+
+Pythonic command line arguments parser, that will make you smile.
+
+-  http://docopt.org
+-  https://github.com/docopt/docopt
+
+
+py.test
+-------
+
+unit tests
+- http://pytest.org/latest/
+
+python-dateutil
+---------------
+
+datetime parsing and calendars
+
+-  https://labix.org/python-dateutil
+-  https://dateutil.readthedocs.org
+
+
+pytz
+----
+
+- World Timezone Definitions for Python http://pytz.sourceforge.net/
+
+
+tldextract
+----------
+
+Accurately separate the TLD from the registered domain and subdomains of a URL, using the Public Suffix List.
+Split URL into subdomain, domain and suffix.
+
+-  https://github.com/john-kurkowski/tldextract
+
+
+msgpack-python
+--------------
+
+It's like JSON. but fast and small.
+MessagePack is an efficient binary serialization format. It lets you exchange
+data among multiple languages like JSON. But it's faster and smaller. Small
+integers are encoded into a single byte, and typical short strings require only
+one extra byte in addition to the strings themselves.
+
+-  http://msgpack.org/
+-  https://github.com/msgpack/msgpack-python
+
+
+six
+---
+
+-  https://pypi.python.org/pypi/six
+-  https://pythonhosted.org/six/
+
+futures
+-------
+
+-  https://code.google.com/p/pythonfutures/
+-  https://docs.python.org/3/library/concurrent.futures.html
+-  https://docs.python.org/dev/library/concurrent.futures.html
+-  https://pypi.python.org/pypi/futures
+-  https://pythonhosted.org/futures/
 
---
+++ stealfromthis.py
@@ -0,0 +1,86 @@
+#! /usr/bin/env python2
+
+
+# standard library imports
+import os
+import sys
+
+import datetime as dt
+
+
+    import json
+    from collections import namedtuple
+    from collections import defaultdict
+    from collections import deque
+    from itertools import groupby, chain
+    from functools import wraps
+    import argparse
+    import fileinput
+
+    log = logging.getLogger(__name__): good enough for logging
+    from __future__ import absolute_import: fixes import aliasing
+
+
+
+import datetime as dt                     # always import datetime this way
+import json                               # the standard for data interchange
+from collections import namedtuple        # use for lightweight data types
+from collections import defaultdict       # use for counting/grouping
+from collections import deque             # a fast double-ended queue
+from itertools import groupby, chain      # for declarative style
+from functools import wraps               # use for writing well-behaved decorators
+import argparse                           # for "robust" CLI tool building
+import fileinput                          # to create quick UNIX pipe-friendly tools
+
+# log = logging.getLogger(__name__)       # good enough for logging
+# from __future__ import absolute_import  # fixes import aliasing
+
+
+
+# related third party imports
+#
+# common third party libraries
+
+# python-dateutil for datetime parsing and calendars
+# pytz for timezone handling
+# tldextract for better URL handling
+# msgpack-python for a more compact encoding than JSON
+# futures for Future/pool concurrency primitives
+#  docopt for quick throwaway CLI tools
+#  py.test for unit tests, along with mock and hypothesis
+#
+
+
+
+# local application/library specific imports
+#
+#
+
+# no __init__.py in root folder: give your package a folder name!
+# mypackage/__init__.py preferred to src/mypackage/__init__.py
+# mypackage/lib/__init__.py preferred to lib/__init__.py
+# mypackage/settings.py preferred to settings.py
+# README.rst describes the repo for a newcomer; use reST
+# setup.py for simple facilities like setup.py develop
+# requirements.txt describes package dependencies for pip
+# dev-requirements.txt additional dependencies for tests/local
+# Makefile for simple (!!!) build/lint/test/run steps
+
+
+
+
+
+if 0 and 'recommended':
+   dt.datetime.utcnow() # yes
+
+if 0 and 'not recommended':
+   dt.datetime.now()    # no: local time
+
+
+
+
+
+
+if __name__ == "__main__"
+   pass
+
